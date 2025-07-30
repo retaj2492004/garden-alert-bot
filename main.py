43113ef32610dbbcfca1b290b1f6bb117cda7652
retaj2492004 <retajmelhem2492004@gmail.com>
2025-07-30 22:55:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,5 @@ def home():
 
 if __name__ == '__main__':
     threading.Thread(target=run_checker).start()
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
